e97d1aa9456e7df3dc463557006017d36bc06824
Renato Douglas <renatdoug@hotmail.com>
2023-07-07 01:32:24 -0300

    Criando um app chamado recipes

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', home),
+    path('admin/', admin.site.urls),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
