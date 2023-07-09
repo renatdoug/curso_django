d2e6e7889a80042ab14e5034c00b4c3d6f1b2f84
Renato Douglas <renatdoug@hotmail.com>
2023-07-09 17:38:46 -0300

    Uma nova URL para uma nova VIew

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+"""
 from recipes.views import home
-
 
 urlpatterns = [
     path('', home),
+] A importação dessa forma seria mais trabalhosa, pois para importar outros
+teria que fazer um a um como home, contato, etc. Assim importa-se o pacote
+completo """
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'texto': 'tentado aprender Django',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'texto': 'Renato Douglas',
+    })
